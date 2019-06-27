b81103732faf6e27d9881d6b92cb68797120a98f
Andrea Spoldi <andrea.spoldi@docebo.com>
2019-06-27 16:44:30 +0200

    asg to 0.12 - fixes

--- outputs.tf
+++ outputs.tf
@@ -55,7 +55,7 @@ output "autoscaling_group_health_check_type" {
 
 output "security_group_id" {
   description = "ID of the worker nodes Security Group"
-  value       = join("", aws_security_group.default.*.id)
+  value       = [join("", aws_security_group.default.*.id)]
 }
 
 output "security_group_arn" {
@@ -82,4 +82,3 @@ output "config_map_aws_auth" {
   description = "Kubernetes ConfigMap configuration for worker nodes to join the EKS cluster. https://www.terraform.io/docs/providers/aws/guides/eks-getting-started.html#required-kubernetes-configuration-to-join-worker-nodes"
   value       = join("", data.template_file.config_map_aws_auth.*.rendered)
 }
-
