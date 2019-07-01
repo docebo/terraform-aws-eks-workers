edb90e88e3b4934e48f7c503dc7fd8ec766ca35a
Andrea Spoldi <andrea.spoldi@docebo.com>
2019-07-01 17:17:14 +0200

    it is number, not integer

--- variables.tf
+++ variables.tf
@@ -270,7 +270,7 @@ variable "min_elb_capacity" {
 }
 
 variable "wait_for_elb_capacity" {
-  type        = integer
+  type        = number
   description = "Setting this will cause Terraform to wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. Takes precedence over `min_elb_capacity` behavior"
   default     = false
 }
